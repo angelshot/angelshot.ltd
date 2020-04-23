f3982e296959e20f9458a8ef0101e81a12179721
sidnny <sid@sidnny.com>
2020-04-23 15:17:29 -0400

    adding betaSignupCTA

---
+++ src/components/betaSignupCTA.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BetaSignupCTA = () => (
+    <div id="getApp">
+        <h2 class="cta2">Ever needed an out? <span class="nowrap">Get an Angel.</span></h2>
+        <p class="tagline">Discreetly bails you out of bad dates, rides, and everything else.&trade;</p>
+        <a class="button2" href="angelbot.html">Join the beta</a>
+    </div>
+)
+
+export default BetaSignupCTA
