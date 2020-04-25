e57bbaf8b8d13259e78d31e5ee9abe940b9f284b
sidnny <sid@sidnny.com>
2020-04-25 17:17:23 -0400

    updating floral component

--- src/components/floral.js
+++ src/components/floral.js
@@ -2,14 +2,10 @@ import React from "react"
 import classNames from "classnames/bind"
 import styles from "./floral.module.css"
 
-const Floral = ({ children, className, withHero }) => {
-  var cx = classNames.bind(styles)
-  let classes = cx({
-    withHero,
-    className,
-  })
-  console.log("classes", classes)
-  return <section className={classes}>{children}</section>
-}
+var cx = classNames.bind(styles)
+
+const Floral = ({ children, className, withHero }) => (
+  <section className={cx(className, { withHero })}>{children}</section>
+)
 
 export default Floral
