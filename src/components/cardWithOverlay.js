78e71cab85ad7631717a8305fcb1f5858a27fda1
sidnny <sid@sidnny.com>
2020-04-25 23:31:23 -0400

    finished up goal cards

--- src/components/cardWithOverlay.js
+++ src/components/cardWithOverlay.js
@@ -1,8 +1,14 @@
 import React from "react"
 import styles from "./cardWithOverlay.module.css"
 
-const CardWithOverlay = ({ children }) => (
-  <div className={styles.cardWithOverlay}>{children}</div>
+const CardWithOverlay = ({ children, alt }) => (
+  <div className={alt ? styles.cardWithOverlayAlt : styles.cardWithOverlay}>
+    {children}
+  </div>
+)
+
+const CardBody = ({ children }) => (
+  <div className={styles.cardBody}>{children}</div>
 )
 
 const Overlay = ({ children }) => <ul className={styles.overlay}>{children}</ul>
@@ -11,22 +17,24 @@ const OverlayItem = ({ children }) => (
   <li className={styles.overlayItem}>{children}</li>
 )
 
-const Image = ({ srcset, alt }) => (
-  <picture>
-    <source
-      media="(max-width: 1023px)"
-      srcset={`${srcset[0]}, ${srcset[1]} 2x`}
-    />
-    <source
-      media="(min-width: 1024px)"
-      srcset={`${srcset[2]}, ${srcset[3]} 2x`}
-    />
-    <img src={srcset[0]} alt={alt} className={styles.image} />
-  </picture>
+const Image = ({ srcset, alt, attribution }) => (
+  <div className={styles.image}>
+    <picture>
+      <source
+        media="(max-width: 1023px)"
+        srcset={`${srcset[0]}, ${srcset[1]} 2x`}
+      />
+      <source
+        media="(min-width: 1024px)"
+        srcset={`${srcset[2]}, ${srcset[3]} 2x`}
+      />
+      <img src={srcset[0]} alt={alt} />
+    </picture>
+  </div>
 )
 
 const Title = ({ children }) => (
   <h2 className={`title ${styles.title}`}>{children}</h2>
 )
 
-export { CardWithOverlay, Overlay, OverlayItem, Image, Title }
+export { CardWithOverlay, CardBody, Overlay, OverlayItem, Image, Title }
--- src/pages/goals.js
+++ src/pages/goals.js
@@ -6,6 +6,7 @@ import Cite from "../components/cite"
 import Hero from "../components/hero"
 import {
   CardWithOverlay,
+  CardBody,
   Overlay,
   OverlayItem,
   Image,
@@ -37,43 +38,48 @@ export default ({ children }) => (
         ]}
         alt="People at a food establishment"
       />
-      <Title>Increase community engagement & accountability</Title>
-      <p>
-        Sexual violence affects everyone. According to the CDC, 1 in 3 women and
-        1 in 4 men experienced sexual violence involving physical contact during
-        their lifetimes. More than half of sexual assault incidents occurs while
-        the victim is pursuing leisure activities away from home, and instances
-        of rape within colleges are severely under-reported.
-      </p>
-      <h3 className="subheader2">THIS IS NOT OKAY.</h3>
-      <p>
-        The AngelShot WINGS Program will reinforce social norms that protect
-        against sexual violence to achieve our mission of creating safe
-        environments.
-      </p>
-      <p>
-        Our Ambassadors work with local establishments, student councils and
-        school administrators to identify and address sexual violence by
-        defining policies on prevention and intervention within their
-        communities.
-      </p>
-      <p>
-        To be designated as Havens, local establishments and schools are asked
-        to take a pledge of accountability and transparency — self-assessing
-        their current environment when it comes to sexual violence prevention
-        and taking actions to make it safe.
-      </p>
+      <CardBody>
+        <Title>Increase community engagement & accountability</Title>
+        <p>
+          <strong>Sexual violence affects everyone.</strong> According to the
+          CDC, 1 in 3 women and 1 in 4 men experienced sexual violence involving
+          physical contact during their lifetimes. More than half of sexual
+          assault incidents occurs while the victim is pursuing leisure
+          activities away from home, and instances of rape within colleges are
+          severely under-reported.
+        </p>
+        <h3 className="subheader2">THIS IS NOT OKAY.</h3>
+        <p>
+          The AngelShot WINGS Program will reinforce social norms that protect
+          against sexual violence to achieve our mission of creating safe
+          environments.
+        </p>
+        <p>
+          Our Ambassadors work with local establishments, student councils and
+          school administrators to identify and address sexual violence by
+          defining policies on prevention and intervention within their
+          communities.
+        </p>
+        <p>
+          To be designated as Havens, local establishments and schools are asked
+          to take a pledge of accountability and transparency — self-assessing
+          their current environment when it comes to sexual violence prevention
+          and taking actions to make it safe.
+        </p>
+      </CardBody>
       <Overlay>
         <OverlayItem>
-          51% of student rape and sexual assault victimizations occurred while
-          the victim was pursuing leisure activities away from home.
+          51% of student rape and sexual assault{" "}
+          <strong>victimizations occurred</strong> while the victim was pursuing
+          leisure activities <strong>away from home</strong>.
         </OverlayItem>
         <OverlayItem>
-          89% of college campuses reported zero incidents of rape in 2016.
+          89% of college campuses <strong>reported zero incidents</strong> of
+          rape in 2016.
         </OverlayItem>
       </Overlay>
     </CardWithOverlay>
-    <CardWithOverlay>
+    <CardWithOverlay alt>
       <Image
         srcset={[
           "/img/goal-seek_help-small.png",
@@ -83,40 +89,44 @@ export default ({ children }) => (
         ]}
         alt="People sitting in a support circle"
       />
-      <Title>Prevent incidents and encourage victims to seek help</Title>
-      <p>
-        As awareness becomes more prevalent, we are seeing an increased number
-        of grassroots movements within individual establishments that help
-        prevent people from becoming victims of sexual violence. While
-        technology has enabled us to easily check in on our friends when they’re
-        out on dates, it still requires relying on someone else. Not everyone
-        will have that option and there is no guarantee someone will be around
-        when you need them.
-      </p>
-      <h3 className="subheader2">THIS NEEDS TO BE BETTER.</h3>
-      <p>
-        AngelShot’s knowledge base will arm an individual with the information
-        and knowledge to recognize when they’re in a potentially dangerous
-        situation, and will provide them with the methodology to help themselves
-        get out of it.
-      </p>
-      <p>
-        Using a mobile phone, AngelBot provides an exit strategy for an
-        individual when they need to be bailed out of an unwanted situation.
-      </p>
-      <p>
-        Should someone become the victim of sexual violence, we will be there to
-        provide the support and resources necessary for them to report the
-        incident.
-      </p>
+      <CardBody>
+        <Title>Prevent incidents and encourage victims to seek help</Title>
+        <p>
+          As awareness becomes more prevalent, we are seeing an increased number
+          of grassroots movements within individual establishments that help
+          prevent people from becoming victims of sexual violence. While
+          technology has enabled us to easily check in on our friends when
+          they’re out on dates, it still requires relying on someone else. Not
+          everyone will have that option and there is no guarantee someone will
+          be around when you need them.
+        </p>
+        <h3 className="subheader2">THIS NEEDS TO BE BETTER.</h3>
+        <p>
+          AngelShot’s knowledge base will arm an individual with the information
+          and knowledge to recognize when they’re in a potentially dangerous
+          situation, and will provide them with the methodology to help
+          themselves get out of it.
+        </p>
+        <p>
+          Using a mobile phone, AngelBot provides an exit strategy for an
+          individual when they need to be bailed out of an unwanted situation.
+        </p>
+        <p>
+          Should someone become the victim of sexual violence, we will be there
+          to provide the support and resources necessary for them to report the
+          incident.
+        </p>
+      </CardBody>
       <Overlay>
         <OverlayItem>
-          80% of rape and sexual assault victimizations of students and 67% of
-          non-students go unreported to police.
+          80% of rape and <strong>sexual assault victimizations</strong> of
+          students and 67% of non-students go{" "}
+          <strong>unreported to police</strong>.
         </OverlayItem>
         <OverlayItem>
-          Fewer than 1 in 5 female victims received assistance from a victim
-          services agency.
+          <strong>Fewer</strong> than 1 in 5 female victims{" "}
+          <strong>received assistance</strong>
+          from a victim services agency.
         </OverlayItem>
       </Overlay>
     </CardWithOverlay>
@@ -130,41 +140,44 @@ export default ({ children }) => (
         ]}
         alt="Man holding a woman's hand down on a cafe table"
       />
-      <Title>Prevent violence by ending “Rape Culture”</Title>
-      <p>
-        We live in a society where sexual violence is normalized and excused in
-        the media. It is bolstered in popular culture by the objectification of
-        women’s bodies, use of misogynistic language, and the romanticization of
-        sexual violence. This encroaches on women’s civil liberties from
-        personal interactions to governmental policies.
-      </p>
-      <h3 className="subheader2">THIS HAS TO CHANGE.</h3>
-      <p>
-        Women’s rights and safety should be a guarantee. AngelShot provides
-        educational resources that will compel our society to shift its mindset
-        on the unacceptable behaviors that perpetuate rape culture. Through
-        shared stories and experiences, we provide an outlet for those who have
-        been a victim of sexual violence.
-      </p>
-      <p>
-        We must educate and empower our communities to identify and combat the
-        culture of toxic behavior. This includes engaging allies on how to be
-        respectful sexual partners, promoting healthy sexuality and intimate
-        relationship skills.
-      </p>
-      <p>
-        Events allow our program to grow and reach more women and communities so
-        that they can take an active role to influence and ameliorate these
-        cultural norms.
-      </p>
+      <CardBody>
+        <Title>Prevent violence by ending “Rape Culture”</Title>
+        <p>
+          We live in a society where sexual violence is normalized and excused
+          in the media. It is bolstered in popular culture by the
+          objectification of women’s bodies, use of misogynistic language, and
+          the romanticization of sexual violence. This encroaches on women’s
+          civil liberties from personal interactions to governmental policies.
+        </p>
+        <h3 className="subheader2">THIS HAS TO CHANGE.</h3>
+        <p>
+          Women’s rights and safety should be a guarantee. AngelShot provides
+          educational resources that will compel our society to shift its
+          mindset on the unacceptable behaviors that perpetuate rape culture.
+          Through shared stories and experiences, we provide an outlet for those
+          who have been a victim of sexual violence.
+        </p>
+        <p>
+          We must educate and empower our communities to identify and combat the
+          culture of toxic behavior. This includes engaging allies on how to be
+          respectful sexual partners, promoting healthy sexuality and intimate
+          relationship skills.
+        </p>
+        <p>
+          Events allow our program to grow and reach more women and communities
+          so that they can take an active role to influence and ameliorate these
+          cultural norms.
+        </p>
+      </CardBody>
       <Overlay>
         <OverlayItem>
-          In about 80% of rape and sexual assault victimizations, the offender
-          was known to the victim
+          In about 80% of rape and sexual assault victimizations, the{" "}
+          <strong>offender was known</strong> to the victim.
         </OverlayItem>
         <OverlayItem>
           About a quarter of victims who did not report to police believed the
-          incident was a personal matter, and 1 in 5 stated a fear of reprisal.
+          incident was a personal matter, and 1 in 5 stated a{" "}
+          <strong>fear of reprisal</strong>.
         </OverlayItem>
       </Overlay>
     </CardWithOverlay>
