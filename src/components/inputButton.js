38d9e66100d514c8d31d3a50134147c108ccd815
sidnny <sid@sidnny.com>
2020-05-05 15:18:27 -0400

    removing redundant inputButton component

--- src/components/inputButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./inputButton.module.css"
-
-const InputButton = ({ type, value, name }) => (
-  <input type={type} value={value} name={name} className={styles.inline} />
-)
-
-export default InputButton
