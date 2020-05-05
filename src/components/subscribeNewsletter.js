2fac3acf4267e30c5ebe439d78b26e2635a2ad2b
sidnny <sid@sidnny.com>
2020-05-05 15:21:23 -0400

    fixing footer newsletter subheader

--- src/components/subscribeNewsletter.js
+++ src/components/subscribeNewsletter.js
@@ -5,7 +5,9 @@ import styles from "./subscribeNewsletter.module.css"
 
 const SubscribeNewsletter = ({ style }) => (
   <div className={styles.subscribeNewsletter} style={style}>
-    <h6 className={styles.subheader}>Subscribe to our newsletter</h6>
+    <h6 className={`subheader ${styles.subheader}`}>
+      Subscribe to our newsletter
+    </h6>
     <p>
       Stay up to date on the progress of our programs, and discover upcoming
       events and volunteer opportunities.
