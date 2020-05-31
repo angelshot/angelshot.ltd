d27e44806ee884a83d32a032c11f68dd37d08dca
Wendy <nub4life@users.noreply.github.com>
2020-05-31 19:06:28 -0400

    react stuff.

--- src/pages/signup-ty.js
+++ src/pages/signup-ty.js
@@ -6,36 +6,41 @@ import Floral from "../components/floral"
 
 import styles from "./signup-ty.module.css"
 
-function getType() {
-  const type = new URLSearchParams(window.location.search).get('type');
+class SignUpTy extends React.Component {
+  _getType() {
+    const type = new URLSearchParams(window.location.search).get('type');
 
-  switch (type) {
-    case "angelbot":
-      return <p>Thank you for signing up for the AngelBot Beta program. We'll be in touch with you shortly!</p>
-    case "wings":
-      return <p>Thank you for your interest in joining the AngelShot WINGS Program. We'll be in touch with you shortly!</p>
-    case "newsletter":
-      return <p>Thank you for subscribing to our newsletter. Keep an eye out for upcoming events and volunteer opportunities!</p>
-    default:
-      return <p>You're in! Thanks for signing up.</p>
+    switch (type) {
+      case "angelbot":
+        return <p>Thank you for signing up for the AngelBot Beta program. We'll be in touch with you shortly!</p>
+      case "wings":
+        return <p>Thank you for your interest in joining the AngelShot WINGS Program. We'll be in touch with you shortly!</p>
+      case "newsletter":
+        return <p>Thank you for subscribing to our newsletter. Keep an eye out for upcoming events and volunteer opportunities!</p>
+      default:
+        return <p>You're in! Thanks for signing up.</p>
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <Hero className={styles.hero}>
+          <Intro
+            quoteStyles={styles.quote}
+            quote="The greatness of a community is most accurately measured by the compassionate actions of its members."
+            quoteAuthor="Coretta Scott King"
+          >
+            Thank you!
+          </Intro>
+        </Hero>
+        <Floral withHero className={styles.section}>      
+          <div>        
+            {this._getType()}
+          </div>      
+        </Floral>
+      </Layout>
+    )
   }
 }
 
-export default () => (
-  <Layout>
-    <Hero className={styles.hero}>
-      <Intro
-        quoteStyles={styles.quote}
-        quote="The greatness of a community is most accurately measured by the compassionate actions of its members."
-        quoteAuthor="Coretta Scott King"
-      >
-        Thank you!
-      </Intro>
-    </Hero>
-    <Floral withHero className={styles.section}>      
-      <div>        
-        {getType()}
-      </div>      
-    </Floral>
-  </Layout>
-)
+export default SignUpTy
