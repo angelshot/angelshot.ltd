e3ae20215d20c494249570168ecc2346c13eb6f0
sidnny <sid@sidnny.com>
2020-07-07 14:49:36 -0400

    moved volunteer sign up thank you page back to the root

--- src/pages/volunteer-ty.js
+++ src/pages/volunteer-ty.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Layout from "../../components/layout"
-import Intro from "../../components/intro"
-import Hero from "../../components/hero"
-import Floral from "../../components/floral"
+import Layout from "../components/layout"
+import Intro from "../components/intro"
+import Hero from "../components/hero"
+import Floral from "../components/floral"
 
 import styles from "./volunteer-ty.module.css"
 
