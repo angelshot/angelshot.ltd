805058d2dc1d9504a4552baeec4363bab4016305
sidnny <sid@sidnny.com>
2020-07-07 13:08:42 -0400

    adding new content

--- src/pages/get-involved.js
+++ src/pages/get-involved.js
@@ -4,8 +4,7 @@ import Intro from "../components/intro"
 import Hero from "../components/hero"
 import Floral from "../components/floral"
 import Button from "../components/button"
-import InputCheckbox from "../components/inputCheckbox"
-import InputText, { InlineInputText } from "../components/inputText"
+import { Card } from "../components/card"
 
 import styles from "./get-involved.module.css"
 
@@ -21,109 +20,24 @@ export default () => (
       </Intro>
     </Hero>
     <Floral withHero className={styles.section}>
-      <div>
-        <h2 className="title">Available Opportunities</h2>
+      <h2 className="title">How you can help</h2>
+      <p>
+        You can make a real impact to improve the safety of women everywhere!
+        <br />
+        By volunteering your time, there are many ways you can help further our
+        cause.
+      </p>
+      <Card>
+        <h3 className="subheader">Become a donor</h3>
+        <img src="/icons/noun_Donate_29964-modified-color.svg" />
         <p>
-          Thank you for your interest in helping AngelShot Ltd. achieve our
-          mission of providing tools and knowledge that empower women to
-          safeguard themselves and encouraging communities to act as havens for
-          them. As a small and mighty team, we could use your expertise in the
-          following areas:
+          With your donation, you will help fund the{" "}
+          <strong>development of new tools and programs</strong> that can help
+          prevent sexual harassment and violence via community engagement and
+          accountability.
         </p>
-        <h3 className="subheader">Creative &amp; User Experience</h3>
-        <p>
-          We are looking for copywriters and user experience designers to help
-          refine the dialog within AngelBot, create newsletter templates,
-          fundraising campaigns, and social media content.
-        </p>
-        <h3 className="subheader">Technology</h3>
-        <p>
-          We are currently accepting engineering volunteers to work on the
-          AngelBot Beta and our website. Our stack consists of NodeJS, GCP,
-          DialogFlow, Docker, and Gatsby.js. You will be working on features
-          such as website updates, user management, privacy &amp; security, and
-          improving upon our infrastructure.
-        </p>
-        <h3 className="subheader">Outreach &amp; Fundraising</h3>
-        <p>
-          As a non-profit, grants and donations are our main sources of funding.
-          We are seeking volunteers who can help us organize fundraising
-          campaigns, and apply for grants.
-        </p>
-        <h3 className="subheader">Other</h3>
-        <p>
-          As a designated 501(c)3 organization, we welcome any pro-bono
-          professional services such as legal and bookkeeping.
-        </p>
-      </div>
-      <div className={styles.volunteerForm}>
-        <form
-          action="https://ltd.us4.list-manage.com/subscribe/post"
-          method="POST"
-        >
-          <h3 className="subtitle">Volunteer with Us</h3>
-          <input type="hidden" name="u" value="091175bb199db49734caae8f1" />
-          <input type="hidden" name="id" value="869d816545" />
-          <h4 className={`subheader2 ${styles.subheader}`}>Required fields:</h4>
-          <InputText
-            type="text"
-            name="NAME"
-            placeholder="Name"
-            required
-            labelBody="Name"
-          />
-          <InputText
-            type="email"
-            name="EMAIL"
-            placeholder="Email"
-            required
-            labelBody="Email"
-          />
-          <h4 className={`subheader2 ${styles.subheader}`}>
-            I am interested in volunteering my services to:
-          </h4>
-          <InputCheckbox
-            type="checkbox"
-            value="crux"
-            name="group[70176][1]"
-            id="group_1"
-          >
-            Creative &amp; User Experience
-          </InputCheckbox>
-          <InputCheckbox
-            type="checkbox"
-            value="tech"
-            name="group[70176][2]"
-            id="group_2"
-          >
-            Technology
-          </InputCheckbox>
-          <InputCheckbox
-            type="checkbox"
-            value="outreach"
-            name="group[70176][3]"
-            id="group_3"
-          >
-            Outreach &amp; Fundraising
-          </InputCheckbox>
-          <InputCheckbox
-            type="checkbox"
-            value="other"
-            name="group[70176][4]"
-            id="group_4"
-          >
-            Other
-            <InlineInputText type="text" name="OTHER" inline />
-          </InputCheckbox>
-          <Button
-            className={styles.submit}
-            buttonType="buttonWide"
-            type="submit"
-            tagName="button"
-            children="Volunteer"
-          />
-        </form>
-      </div>
+        <Button href="#donate">Donate today</Button>
+      </Card>
     </Floral>
   </Layout>
 )
