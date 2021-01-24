178f4d9c9527ddda124fd939789ac3e282dd79be
sidnny <sid@sidnny.com>
2021-01-24 18:43:51 -0500

    making it longer

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -12,7 +12,7 @@ export default () => (
       <Intro>Privacy Policy</Intro>
     </Hero>
     <Floral withHero>
-      <iframe src="https://docs.google.com/document/d/e/2PACX-1vS5I_s12XVhb_eEx9AlSD33MLHXfqMSR3U3HyP2EVlWfZCoazxdcu5cpcPhOAcWU_OaoFyWRGTNfu12/pub?embedded=true"></iframe>
+      <iframe height="2500" src="https://docs.google.com/document/d/e/2PACX-1vS5I_s12XVhb_eEx9AlSD33MLHXfqMSR3U3HyP2EVlWfZCoazxdcu5cpcPhOAcWU_OaoFyWRGTNfu12/pub?embedded=true"></iframe>
     </Floral>
   </Layout>
 )
